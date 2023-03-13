c96e387139e35a7b3f6cfd6dd53f3622a6c47efd
Hayden Young <haydenyoung@wijiti.com>
2023-03-13 17:21:14 +0000

    test: Check for heads paths.

--- test/database.test.js
+++ test/database.test.js
@@ -1,6 +1,6 @@
 import { strictEqual, deepStrictEqual } from 'assert'
 import rmrf from 'rimraf'
-import { copy } from 'fs-extra'
+import { copy, exists } from 'fs-extra'
 import * as IPFS from 'ipfs'
 import Path from 'path'
 import { Log, Entry, Database, KeyStore, Identities } from '../src/index.js'
@@ -53,11 +53,11 @@ describe('Database', function () {
   })
 
   afterEach(async () => {
-    await rmrf('./orbitdb1')
+    await rmrf('./orbitdb')
   })
 
   it('adds an operation', async () => {
-    db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb1' })
+    db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb' })
     const expected = 'zdpuAqQ9TJpMhPShuT315m2D9LUBkBPy8YX9zatjEynd2suZv'
     const op = { op: 'PUT', key: 1, value: 'record 1 on db 1' }
     const actual = await db.addOperation(op)
@@ -73,18 +73,44 @@ describe('Database', function () {
       const op = { op: 'PUT', key: 1, value: 'record 1 on db 1' }
       const hash = await db.addOperation(op)
 
+      const headsPath = Path.join('./orbitdb/', `${databaseId}/`, '/log/_heads/')
+
+      strictEqual(await exists(headsPath), true)
+
       await db.close()
 
-      const headsStorage = await LevelStorage({ path: Path.join('./orbitdb', `./${databaseId}/`, '/log/_heads/') })
+      const headsStorage = await LevelStorage({ path: headsPath })
 
       deepStrictEqual((await Entry.decode(await headsStorage.get(hash))).payload, op)
 
       await headsStorage.close()
+
+      await rmrf(headsPath)
+    })
+
+    it('uses given directory for headsStorage', async () => {
+      db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './custom-directory' })
+      const op = { op: 'PUT', key: 1, value: 'record 1 on db 1' }
+      const hash = await db.addOperation(op)
+
+      const headsPath = Path.join('./custom-directory/', `${databaseId}/`, '/log/_heads/')
+
+      strictEqual(await exists(headsPath), true)
+
+      await db.close()
+
+      const headsStorage = await LevelStorage({ path: headsPath })
+
+      deepStrictEqual((await Entry.decode(await headsStorage.get(hash))).payload, op)
+
+      await headsStorage.close()
+
+      await rmrf(headsPath)
     })
 
     it('uses given MemoryStorage for headsStorage', async () => {
       const headsStorage = await MemoryStorage()
-      db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb1', headsStorage })
+      db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb', headsStorage })
       const op = { op: 'PUT', key: 1, value: 'record 1 on db 1' }
       const hash = await db.addOperation(op)
 
@@ -95,7 +121,7 @@ describe('Database', function () {
 
     it('uses given MemoryStorage for entryStorage', async () => {
       const entryStorage = await MemoryStorage()
-      db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb1', entryStorage })
+      db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb', entryStorage })
       const op = { op: 'PUT', key: 1, value: 'record 1 on db 1' }
       const hash = await db.addOperation(op)
 
@@ -107,7 +133,7 @@ describe('Database', function () {
 
   describe('Events', () => {
     beforeEach(async () => {
-      db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb1' })
+      db = await Database({ OpLog, ipfs, identity: testIdentity, address: databaseId, accessController, directory: './orbitdb' })
     })
 
     it('emits \'close\' when the database is closed', async () => {
