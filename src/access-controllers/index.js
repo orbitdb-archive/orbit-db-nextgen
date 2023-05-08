dd63e4ebd22ad0a307e45675988256f42070fc11
Hayden Young <haydenyoung@wijiti.com>
2023-05-09 00:14:41 +0100

    docs: API for Access controller management.

--- src/access-controllers/index.js
+++ src/access-controllers/index.js
@@ -1,12 +1,27 @@
-/** @module AccessControllers */
+/**
+ * @module AccessControllers
+ * @description
+ * Provides a platform for managing access controllers.
+ */
 import IPFSAccessController from './ipfs.js'
 import OrbitDBAccessController from './orbitdb.js'
 
+/**
+ * An array of available access controller types.
+ * @name types
+ * @†ype []
+ * @return [] An array of access controller types.
+ */
 const types = {
   ipfs: IPFSAccessController,
   orbitdb: OrbitDBAccessController
 }
 
+/**
+ * Gets an access controller module specified by type.
+ * @param {String} type A valid access controller type.
+ * @return {AccessController} The access controller module.
+ */
 const get = (type) => {
   if (!isSupported(type)) {
     throw new Error(`AccessController type '${type}' is not supported`)
@@ -14,10 +29,25 @@ const get = (type) => {
   return types[type]
 }
 
+/**
+ * Checks whether the access controller exists.
+ * @param {String} type A valid access controller type.
+ * @return {bool} True if the access controller exists, false otherwise.
+ */
 const isSupported = type => {
   return Object.keys(types).includes(type)
 }
 
+/**
+ * Adds an access controller module to the list of supported access controller 
+ * types.
+ * @param {AccessController} accessController A compatible access controller
+ * module.
+ * @throws Access controller `type` already added if the access controller is 
+ * already supported.
+ * @throws Given AccessController class needs to implement: type.if the access 
+ * controller module does not implement a type property.
+ */
 const add = (accessController) => {
   if (types[accessController.type]) {
     throw new Error(`Access controller '${accessController.type}' already added.`)
@@ -30,6 +60,10 @@ const add = (accessController) => {
   types[accessController.type] = accessController
 }
 
+/**
+ * Removes an access controller from the types list.
+ * @param {String} type A valid access controller type.
+ */
 const remove = type => {
   delete types[type]
 }
